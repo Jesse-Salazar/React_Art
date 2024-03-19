51cd446d4ad0cf1d9c4e7f46af2d7e5461cc803c
Jesse Salazar <jsn4991@yahoo.com>
2024-03-19 15:56:59 -0500

    First commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+export default function Input({ label, invalid,...props }) {
+let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+let inputClasses = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"
+
+if (invalid) {
+    labelClasses += " text-red-400"
+} else {
+    labelClasses += " text-stone-300"
+}
+
+  return (
+    <p>
+<label className={labelClasses}>{label}</label>
+    <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300" {...props} />
+  </p>
+  );
+}
